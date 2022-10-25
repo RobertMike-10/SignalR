4ba75f12f761de8e79cf6cac2751575638e9693d
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2022-10-25 09:47:49 -0500

    Added count of views

--- WebApplicationSignalR/WebApplicationSignalR/Hubs/UserHub.cs
+++ WebApplicationSignalR/WebApplicationSignalR/Hubs/UserHub.cs
@@ -5,6 +5,7 @@ namespace WebApplicationSignalR.Hubs
     public class UserHub: Hub
     {
         public static int TotalViews { get; set; } = 0;
+        public static int TotalUsers { get; set; } = 0;
 
         public async Task NewWindowLoaded()
         {
---
+++ WebApplicationSignalR/WebApplicationSignalR/wwwroot/js/usersCount.js
@@ -0,0 +1,25 @@
+﻿
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//recibing data from Hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    console.log("New data:" + value);
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerHTML = value;
+});
+
+//sending data from client
+function newWindowLoaded() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+function onSuccess() {
+    console.log("Connection successful");
+    newWindowLoaded();
+}
+
+function onFailure() {
+    console.log("Error on connection");
+}
+
+connectionUserCount.start().then(onSuccess, onFailure);
