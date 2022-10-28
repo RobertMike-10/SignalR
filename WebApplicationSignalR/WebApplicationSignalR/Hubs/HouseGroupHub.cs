b8ba6a66bf96884e164184e5a201090fd0dfa0a6
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2022-10-28 10:19:41 -0500

    Groups

---
+++ WebApplicationSignalR/WebApplicationSignalR/Hubs/HouseGroupHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplicationSignalR.Hubs
+{
+    public class HouseGroupHub:Hub
+    {
+        public static List<string> GroupsJoined { get; set; } = new();
+
+        public async Task JoinHouse(string house)
+        {
+            var key = Context.ConnectionId + ":" + house;
+            if (!GroupsJoined.Contains(key))
+            {
+                GroupsJoined.Add(key);
+                await Groups.AddToGroupAsync(Context.ConnectionId, house);
+            }
+
+        }
+    }
+}
