0e853b4e97f2e3f80ce5eea7d86edef18670064b
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2022-11-03 17:14:14 -0600

    JoinHouse

--- WebApplicationSignalR/WebApplicationSignalR/Hubs/HouseGroupHub.cs
+++ WebApplicationSignalR/WebApplicationSignalR/Hubs/HouseGroupHub.cs
@@ -12,9 +12,25 @@ namespace WebApplicationSignalR.Hubs
             if (!GroupsJoined.Contains(key))
             {
                 GroupsJoined.Add(key);
-                await Groups.AddToGroupAsync(Context.ConnectionId, house);
+                await Task.WhenAll(Clients.Caller.SendAsync("subscriptionStatus", HouseList(GroupsJoined), house, true),
+                             Clients.Others.SendAsync("memberAddedToHouse", house),
+                             Groups.AddToGroupAsync(Context.ConnectionId, house));
+                
             }
 
+        }
+
+        private string HouseList(List<string> groups)
+        {
+            string result="";
+            foreach (var str in groups)
+            {
+                if(str.Contains(Context.ConnectionId))
+                {
+                    result += str.Split(':')[1] + " ";
+                }
+            }
+            return result;
         }
 
         public async Task LeaveHouse(string house)
@@ -23,7 +39,10 @@ namespace WebApplicationSignalR.Hubs
             if (GroupsJoined.Contains(key))
             {
                 GroupsJoined.Remove(key);
-                await Groups.RemoveFromGroupAsync(Context.ConnectionId, house);
+                await Task.WhenAll(Clients.Caller.SendAsync("subscriptionStatus", HouseList(GroupsJoined), house, false),
+                             Groups.RemoveFromGroupAsync(Context.ConnectionId, house),
+                             Clients.Others.SendAsync("memberRemovedFromHouse", house));
+
             }
 
         }
--- WebApplicationSignalR/WebApplicationSignalR/wwwroot/js/houseGroup.js
+++ WebApplicationSignalR/WebApplicationSignalR/wwwroot/js/houseGroup.js
@@ -2,7 +2,7 @@
 var connectionHouseGroup = new signalR.HubConnectionBuilder().withUrl("/hubs/houseGroup").build();
 let btn_un_gryffindor = document.getElementById("btn_un_gryffindor");
 let btn_un_slytherin = document.getElementById("btn_un_slytherin");
-let btn_un_hufflepuff = document.getElementById("btn_un_slytherin");
+let btn_un_hufflepuff = document.getElementById("btn_un_hufflepuff");
 let btn_un_ravenclaw = document.getElementById("btn_un_ravenclaw");
 let btn_gryffindor = document.getElementById("btn_gryffindor");
 let btn_slytherin = document.getElementById("btn_slytherin");
@@ -13,6 +13,8 @@ let trigger_gryffindor = document.getElementById("trigger_gryffindor");
 let trigger_slytherin = document.getElementById("trigger_slytherin");
 let trigger_hufflepuff = document.getElementById("trigger_hufflepuff");
 let trigger_ravenclaw = document.getElementById("trigger_ravenclaw");
+
+let lbl_houseJoined = document.getElementById("lbl_houseJoined");
 
 btn_gryffindor.addEventListener("click", function (event) {
     event.preventDefault();
@@ -50,8 +52,63 @@ btn_un_hufflepuff.addEventListener("click", function (event) {
 });
 btn_un_ravenclaw.addEventListener("click", function (event) {
     event.preventDefault();
-    connectionHouseGroup.send("LeaveHouse", "Ravenclaw.");
+    connectionHouseGroup.send("LeaveHouse", "Ravenclaw");
 });
+
+connectionHouseGroup.on("memberAddedToHouse", (houseName) => {
+    toastr.success(`Member has Subscribed to ${houseName}`);
+})
+connectionHouseGroup.on("memberRemovedFromHouse", (houseName) => {
+    toastr.warning(`Member has unsubscribed from ${houseName}`);
+})
+
+
+connectionHouseGroup.on("subscriptionStatus", (strGroupsJoined, houseName, hasSubscribed) => {
+    lbl_houseJoined.innerText = strGroupsJoined;
+    if (hasSubscribed) {
+        switch (houseName) {
+            case "Gryffindor":
+                btn_gryffindor.style.display = "none";
+                btn_un_gryffindor.style.display = "";
+                break;
+            case "Hufflepuff":
+                btn_hufflepuff.style.display = "none";
+                btn_un_hufflepuff.style.display = "";
+                break;
+            case "Ravenclaw":
+                btn_ravenclaw.style.display = "none";
+                btn_un_ravenclaw.style.display = "";
+                break;
+            case "Slytherin":
+                btn_slytherin.style.display = "none";
+                btn_un_slytherin.style.display = "";
+                break;
+        }
+        toastr.success(`You have Subscribed Successfully. ${houseName}`);
+    }
+    else {
+        switch (houseName) {
+            case "Gryffindor":
+                btn_gryffindor.style.display = "";
+                btn_un_gryffindor.style.display = "none";
+                break;
+            case "Hufflepuff":
+                btn_hufflepuff.style.display = "";
+                btn_un_hufflepuff.style.display = "none";
+                break;
+            case "Ravenclaw":
+                btn_ravenclaw.style.display = "";
+                btn_un_ravenclaw.style.display = "none";
+                break;
+            case "Slytherin":
+                btn_slytherin.style.display = "";
+                btn_un_slytherin.style.display = "none";
+                break;
+        }
+        toastr.success(`You have UnSubscribed Successfully. ${houseName}`);
+    }
+});
+
 function onSuccess() {
 
     console.log("Connection successful");
