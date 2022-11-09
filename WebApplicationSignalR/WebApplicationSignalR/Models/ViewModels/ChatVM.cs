1ce37023ceae216986753c53d1162e227e64e1ff
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2022-11-09 17:24:45 -0600

    ChatVM

---
+++ WebApplicationSignalR/WebApplicationSignalR/Models/ViewModels/ChatVM.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplicationSignalR.Models.ViewModels
+{
+    public class ChatVM
+    {
+        public IList<ChatRoom> Rooms { get;set; }
+        public int MaxRoomAllowed { get; set; }
+        public string? UserId { get; set; }
+
+        public ChatVM()
+        {
+            Rooms = new List<ChatRoom>();
+        }
+
+        public bool AllowAddRom => Rooms == null || Rooms.Count < MaxRoomAllowed;
+    }
+}
