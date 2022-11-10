1640440ffd6ff7f8dfa45b938bcdb365e08a47ee
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2022-11-10 11:58:28 -0600

    ConnectionHub

--- WebApplicationSignalR/WebApplicationSignalR/Hubs/ChatHub.cs
+++ WebApplicationSignalR/WebApplicationSignalR/Hubs/ChatHub.cs
@@ -1,9 +1,31 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using WebApplicationSignalR.Data;
 
 namespace WebApplicationSignalR.Hubs
 {
     public class ChatHub: Hub
     {
+        private readonly ApplicationDbContext _db;
 
+        public ChatHub(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public override Task OnConnectedAsync()
+        {
+            var UserId = Context!.User!.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (!string.IsNullOrEmpty(UserId))
+            {
+                var userName =  _db.Users.FirstOrDefault(u => u.Id == UserId)!.UserName;
+                Clients.Users(HubConnections.OnlineUsers()).SendAsync("ReceiveUserConnected", UserId, 
+                              userName,HubConnections.HasUser(UserId));
+                HubConnections.AddUserConnection(UserId, Context.ConnectionId);
+
+            }
+
+            return base.OnConnectedAsync();
+        }
     }
 }
--- WebApplicationSignalR/WebApplicationSignalR/wwwroot/js/chat.js
+++ WebApplicationSignalR/WebApplicationSignalR/wwwroot/js/chat.js
@@ -1,1 +1,30 @@
-﻿
+﻿var connectionChat = new signalR.HubConnectionBuilder()
+    .withUrl("/hubs/chat")
+    .withAutomaticReconnect([0,1000,5000,10000,null])
+    .build();
+
+connectionChat.on("ReceiveUserConnected", (userId, userName, isOldConnection) => {
+    if (!isOldConnection)
+      addMessage(`${userName} is online`);
+});
+
+
+function addMessage(msg) {
+    if (msg == null && msg == '') {
+        return;
+    }
+    let ui = document.getElementById('messagesList');
+    let li = document.createElement("li");
+    li.innerHTML = msg;
+    ui.appendChild(li);
+}
+
+
+function onSuccess() {
+        console.log("Connection successful");
+}
+
+function onFailure() {
+        console.log("Error on connection");
+}
+connectionChat.start().then(onSuccess, onFailure);
